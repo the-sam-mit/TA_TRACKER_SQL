e6685bfbf499b83bff73502fcb1dffc88673d8d0
Ayush Verma <iit2018185@iiita.ac.in>
2020-09-29 18:53:22 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,8 +2,8 @@
 module.exports = {
     'connection': {
         'host': 'localhost',
-        'user': 'hsuya',
-        'password': 'aviverma123',
+        'user': 'USERNAME', //edit
+        'password': 'PASSWORD', //edit
         'database': 'my_schema2'
     },
     'database': 'my_schema2',
@@ -13,26 +13,6 @@ module.exports = {
     'users_table3': 'asisstant',
     'users_table4': 'assignment',
     'courses': 'course',
-    'rel1': 'takes',
-    'rel2': 'teaches',
-    'rel3': 'under',
-    'rel4': 'assigned'
-};
-// // config/database.js
-// module.exports = {
-//     'connection': {
-//         'host': 'localhost',
-//         'user': 'USERNAME', //edit
-//         'password': 'PASSWORD', //edit
-//         'database': 'my_schema2'
-//     },
-//     'database': 'my_schema2',
-//     'users_table': 'users',
-//     'users_table1': 'professor',
-//     'users_table2': 'student',
-//     'users_table3': 'asisstant',
-//     'users_table4': 'assignment',
-//     'courses': 'course',
 //     'rel1': 'takes',
 //     'rel2': 'teaches',
 //     'rel3': 'under',
